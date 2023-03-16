87a414f15dd7373876548419deb3a3fadbc438a2
heyri.lee <hyeri.l.h@sk.com>
2023-03-16 18:17:16 +0900

    status code error

--- test_client.py
+++ test_client.py
@@ -17,7 +17,7 @@ class testapi:
             'uploadimages'
         ]
         self.count = count
-        self.headers = {'Content-type': 'application/json; charset=utf-8'}
+        self.headers = {'Content-type': 'application/json'}
         self.path = path
     
     def base64_requests(self):
@@ -37,22 +37,7 @@ class testapi:
                 result.append(total_time)
         # return print("Elapsed Time:", total_time)
         return print("Base64 average time:", np.round(sum(result)/self.count,3))
-        
-    # def uploadimage_requests(self):
-    #     url_ = self.host + self.urls[2]
-    #     result = []
-    #     with open(self.path, "rb") as image_file:
-    #         files = {'image': image_file}
-    #         for _ in range(self.count):
-    #             start_time = time.time()
-    #             response = requests.post(url_, files=files)
-    #             assert response.status_code == 200
-    #             end_time = time.time()
-    #             total_time = end_time - start_time
-    #             result.append(total_time)
-    #     # print(response.json())
-    #     return print("uploadFile average time:", np.round(sum(result)/self.count,3))
-       
+           
     def uploadimage_requests(self):
         url_ = self.host + self.urls[2]
         result = []
@@ -84,14 +69,14 @@ class testapi:
             end_time = time.time()
             total_time = end_time - start_time
             result.append(total_time)
-        # return total_time    
+
         return print("image folder average time:", np.round(sum(result)/self.count,3))
 
         
 if __name__ == '__main__':
-    path = "/Users/hyeri/Downloads/images/30_1.jpg"
-    tester = testapi(10, path)
-    # tester.base64_requests()
+    path = "/Users/hyeri/Downloads/real_test_img_crop/testimg.jpeg"
+    tester = testapi(1, path)
+    tester.base64_requests()
     tester.uploadimage_requests()
-    # tester.path_requests()
+    tester.path_requests()
     
